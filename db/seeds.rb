e581e4c919e2b9e7a641817cc04b98079a7148e1
Marc Altmann <marcaltmann@posteo.de>
2019-04-23 14:59:30 +0200

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+thoughts = Array.new
+
+1000.times do
+  thoughts << { content: Faker::Lorem.sentence }
+end
+
+Thought.create(thoughts)
